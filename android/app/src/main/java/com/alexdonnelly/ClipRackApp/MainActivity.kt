71e33132119c929c4fe35ac313e085763937e941
Alex.Donnelly <alex.donnelly@payfirma.com>
2025-08-19 01:07:27 -0400

    Share button working on physical device

--- android/app/src/main/java/com/alexdonnelly/ClipRackApp/MainActivity.kt
+++ android/app/src/main/java/com/alexdonnelly/ClipRackApp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.devcassius.ClipRack
+package com.alexdonnelly.ClipRackApp
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/alexdonnelly/ClipRackApp/MainApplication.kt
+++ android/app/src/main/java/com/alexdonnelly/ClipRackApp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.devcassius.ClipRack
+package com.alexdonnelly.ClipRackApp
 
 import android.app.Application
 import android.content.res.Configuration
